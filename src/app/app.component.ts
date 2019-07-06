d0ac2aa5be2ac763169dc955ddb196ac5d86d399
Christian G. Fritsch <frontend.guilherme@gmail.com>
2019-07-06 18:16:38 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'edl-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'edl-client';
+}
