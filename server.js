c859ecbec29dcd2fcaac7274075e8607bb7456a6
Christian G. Fritsch <christian@rz2.com.br>
2019-07-13 13:27:10 -0300

    modificado o package.json

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+app.use(express.static(`${__dirname}/dist/edl-client`));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+  res.sendFile(path.join(`${__dirname}/dist/edl-client/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
